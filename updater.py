732636c4ac363f1c0106d888f901e57de95e745c
o355 <owen@owenthe.ninja>
2017-03-31 12:15:09 -0400

    More! More! MORE!

--- updater.py
+++ updater.py
@@ -19,6 +19,7 @@ try:
     verbosity = config.getboolean('VERBOSITY', 'updater_verbosity')
     jsonVerbosity = config.getboolean('VERBOSITY', 'updater_jsonverbosity')
     showReleaseTag = config.getboolean('UPDATER', 'show_updaterReleaseTag')
+    tracebacksEnabled = config.getboolean('TRACEBACK', 'updater_tracebacks')
 except:
     print("Couldn't load your config file. Make sure your spelling is correct.")
     print("Setting variables to default...")
@@ -26,27 +27,31 @@ except:
     verbosity = False
     jsonVerbosity = False
     showReleaseTag = False
+    tracebacksEnabled = False
     
     
 import logging
-logger = logging.getLogger('pyweather_updater_0.5beta')
-logger.setLevel(logging.DEBUG)
+logger = logging.getLogger('pyweather_updater_0.5.1beta')
 logformat = '%(asctime)s | %(levelname)s | %(message)s'
 logging.basicConfig(format=logformat)
 
-buildnumber = 50
-buildversion = "0.5 beta"
 if verbosity == True:
-    logger.debug("buildnumber: %s ; buildversion: %s" %
-                (buildnumber, buildversion))
+    logger.setLevel(logging.DEBUG)
+elif tracebacksEnabled == True:
+    logger.setLevel(logging.ERROR)
+else:
+    logger.setLevel(logging.CRITICAL)
+    
+buildnumber = 51
+buildversion = "0.5.1 beta"
+logger.debug("buildnumber: %s ; buildversion: %s" %
+             (buildnumber, buildversion))
 print("Checking for updates. This shouldn't take that long.")
 try:
     versioncheck = urllib.request.urlopen("https://raw.githubusercontent.com/o355/pyweather/master/updater/versioncheck.json")
-    if verbosity == True:
-        logger.debug("versioncheck: %s" % versioncheck)
+    logger.debug("versioncheck: %s" % versioncheck)
 except:
-    if verbosity == True:
-        logger.warn("Couldn't check for updates! Is there an internet connection?")
+    logger.warn("Couldn't check for updates! Is there an internet connection?")
     print(Style.BRIGHT + Fore.RED + "Couldn't check for updates.")
     print("Make sure GitHub user content is unblocked, and you have an internet connection.")
     print("Error 54, updater.py")
@@ -57,27 +62,23 @@ except:
 versionJSON = json.load(reader(versioncheck))
 if jsonVerbosity == True:
     logger.debug("versionJSON: %s" % versionJSON)
-if verbosity == True:
-    logger.debug("Loaded versionJSON with reader %s" % reader)
+logger.debug("Loaded versionJSON with reader %s" % reader)
 version_buildNumber = float(versionJSON['updater']['latestbuild'])
 version_latestVersion = versionJSON['updater']['latestversion']
 version_latestURL = versionJSON['updater']['latesturl']
 version_latestFileName = versionJSON['updater']['latestfilename']
 version_latestReleaseTag = versionJSON['updater']['latestversiontag']
-if verbosity == True:
-    logger.debug("version_buildNumber: %s ; version_latestVersion: %s"
-                % (version_buildNumber, version_latestVersion))
-    logger.debug("version_latestURL: %s ; verion_latestFileName: %s"
-                % (version_latestURL, version_latestFileName))
-    logger.debug("version_latestReleaseTag: %s" % version_latestReleaseTag)
+logger.debug("version_buildNumber: %s ; version_latestVersion: %s"
+             % (version_buildNumber, version_latestVersion))
+logger.debug("version_latestURL: %s ; verion_latestFileName: %s"
+             % (version_latestURL, version_latestFileName))
+logger.debug("version_latestReleaseTag: %s" % version_latestReleaseTag)
 version_latestReleaseDate = versionJSON['updater']['releasedate']
-if verbosity == True:
-    logger.debug("version_latestReleaseDate: %s" % version_latestReleaseDate)
+logger.debug("version_latestReleaseDate: %s" % version_latestReleaseDate)
 if buildnumber >= version_buildNumber:
-    if verbosity == True:
-        logger.info("PyWeather is up to date.")
-        logger.info("local build (%s) >= latest build (%s)"
-                    % (buildnumber, version_buildNumber))
+    logger.info("PyWeather is up to date.")
+    logger.info("local build (%s) >= latest build (%s)"
+                % (buildnumber, version_buildNumber))
     print("")
     print(Style.BRIGHT + Fore.GREEN + "PyWeather is up to date!")
     print("You have version: " + Fore.CYAN + buildversion)
@@ -88,10 +89,9 @@ if buildnumber >= version_buildNumber:
 
 elif buildnumber < version_buildNumber:
     print("")
-    if verbosity == True:
-        logger.warn("PyWeather is NOT up to date.")
-        logger.warn("local build (%s) < latest build (%s)"
-                    % (buildnumber, version_buildNumber))
+    logger.warn("PyWeather is NOT up to date.")
+    logger.warn("local build (%s) < latest build (%s)"
+                % (buildnumber, version_buildNumber))
     print(Fore.RED + Style.BRIGHT + "PyWeather is not up to date! :(")
     print(Fore.RED + "You have version: " + Fore.CYAN + buildversion)
     print(Fore.RED + "The latest version is: " + Fore.CYAN + version_latestVersion)
@@ -102,23 +102,19 @@ elif buildnumber < version_buildNumber:
     print("")
     print(Fore.RED + "Would you like to download the latest version?" + Fore.YELLOW)
     downloadLatest = input("Yes or No: ").lower()
-    if verbosity == True:
-        logger.debug("downloadLatest: %s" % downloadLatest)
+    logger.debug("downloadLatest: %s" % downloadLatest)
     if downloadLatest == "yes":
         print("")
-        if verbosity == True:
-            logger.debug("Downloading latest version...")
+        logger.debug("Downloading latest version...")
         print(Fore.YELLOW + "Downloading the latest version of PyWeather...")
         try:
             with urllib.request.urlopen(version_latestURL) as update_response, open(version_latestFileName, 'wb') as update_out_file:
-                if verbosity == True:
-                    logger.debug("update_response: %s ; update_out_file: %s" %
-                                 (update_response, update_out_file))
+                logger.debug("update_response: %s ; update_out_file: %s" %
+                             (update_response, update_out_file))
                 shutil.copyfileobj(update_response, update_out_file)
         except:
-            if verbosity == True:
-                logger.warn("Couldn't download the latest version!")
-                logger.warn("Is the internet online?")
+            logger.warn("Couldn't download the latest version!")
+            logger.warn("Is the internet online?")
             print(Fore.RED + "Couldn't download the latest version.")
             print("Make sure GitHub user content is unblocked, "
                     + "and you have an internet connection.")
@@ -126,10 +122,9 @@ elif buildnumber < version_buildNumber:
             print("Press enter to exit.")
             input()
             sys.exit()
-            
-        if verbosity == True:
-            logger.debug("Latest version was saved, filename: %s"
-                        % version_latestFileName)
+
+        logger.debug("Latest version was saved, filename: %s"
+                     % version_latestFileName)
         print(Fore.YELLOW + "The latest version of PyWeather was downloaded " +
                     "to the base directory of PyWeather, and saved as " +
                     Fore.CYAN + version_latestFileName + Fore.YELLOW + ".")
@@ -147,25 +142,23 @@ elif buildnumber < version_buildNumber:
         input()
         sys.exit()
     else:
-        if verbosity == True:
-            logger.warn("Input could not be understood!")
+        logger.warn("Input could not be understood!")
         print(Fore.GREEN + "Could not understand what you said.")
         print(Fore.GREEN + "Press enter to exit.")
         input()
         sys.exit()
 else:
-    if verbosity == True:
-        logger.error("PW updater failed. Build comparison below.")
+    logger.warn("PW updater failed. Build comparison below.")
+    try:
+        logger.warn("local build: %s ; updater build: %s"
+                    % (buildnumber, version_buildNumber))
+    except:
+        logger.error("Variables are corrupted, or a typo was made.")
+        logger.error("Trying to list variables 1 more time...")
         try:
-            logger.error("local build: %s ; updater build: %s"
-                        % (buildnumber, version_buildNumber))
+            logger.warn("buildnumber: %s" % buildnumber)
         except:
-            logger.error("Variables are corrupted, or a typo was made.")
-            logger.error("Trying to list variables 1 more time...")
-            try:
-                logger.error("buildnumber: %s" % buildnumber)
-            except:
-                logger.error("Variable buildnumber is corrupt.")
+            logger.warn("Variable buildnumber is corrupt.")
     print(Style.BRIGHT + Fore.RED + "PyWeather Updater ran into an error, and couldn't compare versions.")
     print(Fore.RED + "Error 53, updater.py")
     print("Press enter to exit.")
