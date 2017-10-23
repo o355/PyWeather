43ab7ec9a2d4f577cda9dd0dfd46c704cc5399e0
o355 <owen@owenthe.ninja>
2017-10-23 10:59:56 -0400

    Initial support for custom geocoder API key is done

--- pyweather.py
+++ pyweather.py
@@ -372,12 +372,58 @@ def radar_clearImages():
 
 logger.info("Declaring geocoder type...")
 # Declare geocoder type - Section 12
-if geopyScheme == "https":
+if geopyScheme == "https" and geocoder_customkeyEnabled is False:
     geolocator = GoogleV3(scheme='https')
-    logger.debug("geocoder scheme is now https.")
-elif geopyScheme == "http":
+    logger.debug("geocoder scheme is now https, no custom key")
+elif geopyScheme == "https" and geocoder_customkeyEnabled is True:
+    geolocator = GoogleV3(api_key=geocoder_customkey, scheme='https')
+    logger.debug("geocoder scheme is now https, custom key. Validating...")
+    # Do a warm-up geocode
+    try:
+        location = geolocator.geocode("123 5th Avenue, New York, NY")
+    except:
+        logger.debug("warmup geocode failed.")
+    # Do the actual geocode
+    try:
+        location = geolocator.geocode("123 5th Avenue, New York, NY")
+    except geopy.exc.GeocoderQueryError:
+        logger.debug("API key is invalid, falling back to no API key...")
+        print("Your geocoder API key failed to validate, since it is wrong.",
+              "Falling back to no API key...", sep="\n")
+        printException()
+        geolocator = GoogleV3(scheme='https')
+    except:
+        print("When trying to validate your geocoder API key, something went wrong.",
+              "Falling back to no API key...", sep="\n")
+        printException()
+        logger.debug("Failed to validate API key.")
+        geolocator = GoogleV3(scheme='https')
+elif geopyScheme == "http" and geocoder_customkeyEnabled is False:
     geolocator = GoogleV3(scheme='http')
     logger.debug("geocoder scheme is now http.")
+elif geopyScheme == "http" and geocoder_customkeyEnabled is True:
+    geolocator = GoogleV3(api_key=geocoder_customkey, scheme='http')
+    logger.debug("geocoder scheme is now http, custom key. Validating...")
+    # Do a warm-up geocode
+    try:
+        location = geolocator.geocode("123 5th Avenue, New York, NY")
+    except:
+        logger.debug("warmup geocode failed.")
+    # Do the actual geocode
+    try:
+        location = geolocator.geocode("123 5th Avenue, New York, NY")
+    except geopy.exc.GeocoderQueryError:
+        logger.debug("API key is invalid, falling back to no API key...")
+        print("Your geocoder API key failed to validate, since it is wrong.",
+              "Falling back to no API key...", sep="\n")
+        printException()
+        geolocator = GoogleV3(scheme='http')
+    except:
+        print("When trying to validate your geocoder API key, something went wrong.",
+              "Falling back to no API key...", sep="\n")
+        printException()
+        logger.debug("Failed to validate API key.")
+        geolocator = GoogleV3(scheme='http')
 else:
     print("Geocoder scheme variable couldn't be understood.",
           "Defaulting to the https scheme.", sep="\n")
